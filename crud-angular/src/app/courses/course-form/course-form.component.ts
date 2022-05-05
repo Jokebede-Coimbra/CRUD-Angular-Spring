019bdd558a91d218d486b85186960b76e723a2d6
Coimbra <adsjkbd@gmail.com>
2022-05-05 11:15:23 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../services/courses.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -14,7 +15,8 @@ export class CourseFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar) {
+    private _snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -26,11 +28,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(data => console.log(data), error => this.onError());
+      .subscribe(data => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
   }
 
   private onError() {
--- crud-spring/src/main/java/com/coimbra/controller/CourseController.java
+++ crud-spring/src/main/java/com/coimbra/controller/CourseController.java
@@ -5,9 +5,14 @@ import java.util.List;
 import com.coimbra.model.Course;
 import com.coimbra.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -19,10 +24,17 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    //@RequestMapping(method = RequestMethod.GET)
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+
+        return courseRepository.save(course);
     }
 
 }
